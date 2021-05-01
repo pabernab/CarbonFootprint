4ef730f87a9d73e17f06ecd5c9949865359ee160
Nhan Le <nhan.le01@sap.com>
2021-04-30 17:27:57 -0700

    add transition on pages

--- carbonfootprint/src/App.js
+++ carbonfootprint/src/App.js
@@ -1,60 +1,53 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card } from "react-bootstrap"
-import "bootstrap/dist/css/bootstrap.css"
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
 
-var ReactDOM = require('react-dom');
-
+// Does the user's browser support the HTML5 history API?
+// If the user's browser doesn't support the HTML5 history API then we
+// will force full page refreshes on each page change.
+const supportsHistory = "pushState" in window.history;
 
 function App() {
-
-  const Q1Ref = useRef()
-  const Q2Ref = useRef()
-
-  const [loading, setLoading] = useState(false)
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    //Do the formula
-    setLoading(true)
-    const results = Q1Ref.current.value
-
-    const element = <h1>Results: {results} </h1> ;
-    ReactDOM.render(element, document.getElementById('Results'));
-
-    setLoading(false)
-  }
-
-
   return (
-
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Carbon Footprint Calculator</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>How many vehicles does your household have?</Form.Label>
-              <Form.Control type="Text" ref={Q1Ref} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Do you perform regular Vehicle maintenence on those vehicles?</Form.Label>
-              <Form.Control as="select" ref ={Q2Ref} defaultValue = "Yes">
-                    <option>Yes</option>
-                    <option>No</option>
-                </Form.Control>
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-
-      <div id="Results" className="w-100 text-center mt-2"></div>
-
-    </>
-   
+    <BrowserRouter forceRefresh={!supportsHistory}>
+      <div>
+        <Nav />
+        <main>
+          <Route
+            render={({ location }) => {
+              const { pathname } = location;
+              return (
+                <TransitionGroup>
+                  <CSSTransition
+                    key={pathname}
+                    classNames="page"
+                    timeout={{
+                      enter: 1000,
+                      exit: 1000,
+                    }}
+                  >
+                    <Route
+                      location={location}
+                      render={() => (
+                        <Switch>
+                          <Route exact path="/" component={Home} />
+                          <Route path="/about" component={About} />
+                        </Switch>
+                      )}
+                    />
+                  </CSSTransition>
+                </TransitionGroup>
+              );
+            }}
+          />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
