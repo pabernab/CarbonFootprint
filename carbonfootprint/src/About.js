a16a808f0d4546bb95449410beaf92bd4a7dee48
Ariaz2699 <61758287+Ariaz2699@users.noreply.github.com>
2021-05-04 20:42:24 -0700

    Add files via upload

---
+++ carbonfootprint/src/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Page from "./Page";
+
+export default function About(props) {
+  return (
+    <Page color="#1d2124" background="#f8f9fa">
+      <h1>About</h1>
+
+      <p>
+        Basic vehicle CO2 emissions -  
+         
+        - 219 miles/week is the average national consumption per US vehicle 
+        Emissions = ( [Number of miles driven per week for vehicle #1] * [52 weeks in a year] ) / 
+        ( [vehicle #1 avg household fuel efficiency] * [lbs of CO2 emitted per gallon] * 
+        [emission of greenhouse gases other than CO2] )
+
+        - can multiply by 'percent decrease in fuel efficiency' as well if regular maintenance is not performed on your vehicle. 
+        
+      </p>
+      <p>
+        Home Energy CO2 emissions -  
+         
+        - Gas: 
+          pounds of CO2 per thousand cubic feet of natural gas * 12 months
+            ^^ = (average monthly gas bill / price per thousand cubic feet) 
+        
+        - Electric:
+          (average monthly electric bill / price per kWh) * electricity emission factor * 12 months
+        
+        - Oil:
+          oil emission factor * 12 months 
+          ^^ = (average monthly oil bill / price per gallon)
+
+        - Propane:
+          propane emission factor * 12 months
+          ^^ = (average monthly propane bill / price per gallon)
+
+      </p>
+
+    </Page>
+  );
+}
